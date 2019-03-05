6a658e07979cddd0d475fc27cd4d4db17deda773
Peter Mounir <peter.mounir4@gmail.com>
2019-03-05 13:52:16 +0200

    mvc

---
+++ Todoey/Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Peter Mounir on 3/5/19.
+//  Copyright © 2019 Peter Mounir. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = "New item"
+    var done : Bool = false
+}
